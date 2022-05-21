61acb2dc74a05b1a8abc7a4c6930472be6031b2e
kan-to <bienew311@gmail.com>
2022-05-22 03:01:49 +0630

    Input guess created

--- game.rb
+++ game.rb
@@ -4,3 +4,7 @@ secret_word = words.sample;
 puts "The secret_word is #{secret_word}";
 board = ["_"] * secret_word.size;
 p board;
+
+print "Please input your guess: ";
+guess = gets.chomp;
+p "Your guess is #{guess}";
