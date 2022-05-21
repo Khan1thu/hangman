ec065b935685236cecbcbeb6d98f6444f3191acc
kan-to <bienew311@gmail.com>
2022-05-22 03:07:22 +0630

    board update and live created

--- game.rb
+++ game.rb
@@ -5,6 +5,23 @@ puts "The secret_word is #{secret_word}";
 board = ["_"] * secret_word.size;
 p board;
 
-print "Please input your guess: ";
-guess = gets.chomp;
-p "Your guess is #{guess}";
+while lives > 0 && board.include?("_")
+  print "You have #{lives} lives left. Please guess a letter: "
+  guess = gets.chomp
+  puts "Your guess was: #{guess}"
+
+  if secret_word.include?(guess)
+    secret_word.chars.each_with_index do |c, index|
+      if c.downcase == guess.downcase
+        board[index] = c
+      end
+    end
+  else
+    lives -= 1
+    puts "The word did not include: #{guess}."
+  end
+
+  puts board.join(" ")
+end
+
+puts "The word was: #{ secret_word }"
